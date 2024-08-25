cbac59d46480ab97a68c172b6e19e0a8c29fad57
Ramesh <108254225+JRameshkumar@users.noreply.github.com>
2024-08-25 20:06:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ Request to provide me an ouput response with these 4 headings Detailed Analysis,
 """
 
 #model configuration
-model = genai.GenerativeModel(model_name="gemini-1.5-flash",
+model = genai.GenerativeModel(model_name="Med-Gemini-2D",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
