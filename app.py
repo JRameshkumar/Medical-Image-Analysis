0bd1a582a7dc84b0f816ac435f325a704e10c1ad
Ramesh <108254225+JRameshkumar@users.noreply.github.com>
2024-08-25 20:33:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ Request to provide me an ouput response with these 4 headings Detailed Analysis,
 """
 
 #model configuration
-model = genai.GenerativeModel(model_name="gemini-1.0-pro-vision",
+model = genai.GenerativeModel(model_name="gemini-pro-vision",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
